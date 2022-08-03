d1a3baa365840b2eccc95632ef22d1bf1194be16
itsaalexk <alexkononenko6@protonmail.com>
2022-08-03 20:07:23 +0200

    added functionality : titleCounter()

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ let minute2 = document.querySelector(".minute-2");
 let separator = document.querySelector(".separator");
 let seconds1 = document.querySelector(".seconds-1");
 let seconds2 = document.querySelector(".seconds-2");
-    
+let title = document.querySelector("title");
 let   counterMin1 = 2;
 let   counterMin2 = 5;
 let   counterSec1 = 5;
 let   counterSec2 = 9;
 let click = new Audio("sounds/click.wav")
 
-// MODIFICA EL PROGRESO DEL CRONÓMETRO
+// FUNCION QUE SE EJECUTA AL PULSAR EL BOTON
 
 btnPlay.addEventListener("click",()=>{
     progressBar()
@@ -26,12 +26,12 @@ btnPlay.addEventListener("click",()=>{
         seconds1.innerHTML = counterSec1;
         seconds2.innerHTML = counterSec2;
         counterSec2--
-
+        
         if(counterSec2 < 0){
             counterSec2 = 9;
         }
- 
-
+        
+        titleCounter()
     },1000)
 
     setInterval(()=>{
@@ -45,7 +45,7 @@ btnPlay.addEventListener("click",()=>{
 
     setInterval(()=>{
         counterMin2--;
-
+        
         if(counterMin2 <0){
             counterMin1--
             counterMin2 = 9;
@@ -57,6 +57,7 @@ btnPlay.addEventListener("click",()=>{
 
 })
 
+//ESTA FUNCION MODIFICA LA BARRA DE PROGRESO//
 function progressBar(){
 let progressBar =document.querySelector(".bar");
 let progressCounter = 0;
@@ -67,10 +68,17 @@ let progressCounter = 0;
         progressBar.setAttribute("value",progressCounter)
     },5000)
 
-
-
-
 }
+// Esta función pasa el valor del pomodoro en la pestaña del navegador
+function titleCounter(){
+let stringMin1 =counterMin1.toString();
+let stringMin2 = counterMin2.toString();
+let stringSec1 = counterSec1.toString();
+let stringSec2 = counterSec2.toString();
+    
+title.innerHTML = `${stringMin1}${stringMin2}:${stringSec1}${stringSec2}`
+}
+
 
 
 
